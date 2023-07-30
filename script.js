c95c05becdb36313e603f1dabddd21a8e0166e50
Avi Kapoor <avikapoor244@gmail.com>
2023-07-31 01:30:38 +0530

    Dashboard 1 implemented
    Signed-off-by: Avi Kapoor <avikapoor244@gmail.com>

--- script.js
+++ script.js
@@ -1,33 +1,57 @@
-document.getElementById("registrationForm").addEventListener("submit", function(event) {
-    event.preventDefault();
 
-    // Fetch form input values
-    const fullName = document.getElementById("fullName").value;
-    const email = document.getElementById("email").value;
-    const cvFile = document.getElementById("cv").files[0];
+// <script>
+//             const scriptURL = 'https://script.google.com/macros/s/AKfycbxkgWotCMyvIKoT9aGtPCndZdHtHrq8i972t9CKAVjO6xC7T0I/exec'
+//             const form = document.forms['google-sheet']
+          
+//             form.addEventListener('submit', e => {
+//               e.preventDefault()
+//               fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+//                 .then(response => alert("Thanks for Contacting us..! We Will Contact You Soon..."))
+//                 .catch(error => console.error('Error!', error.message))
+//             })
+//           </script>
 
-    // Perform validation (optional)
-    // For example, you can check the file type and size before submission
 
-    // Submit the form data to the server (you can use AJAX/fetch to send data)
-    // Replace "your-server-endpoint" with the actual URL where you want to handle form data on the server
-    const formData = new FormData();
-    formData.append("fullName", fullName);
-    formData.append("email", email);
-    formData.append("cv", cvFile);
+// =============================================================================
 
-    fetch("your-server-endpoint", {
-        method: "POST",
-        body: formData
-    })
-    .then(response => response.json())
-    .then(data => {
-        // Handle the response from the server, e.g., show a success message
-        alert("Registration successful!");
-        document.getElementById("registrationForm").reset(); // Reset the form
-    })
-    .catch(error => {
-        // Handle errors, e.g., show an error message
-        alert("An error occurred. Please try again later.");
-    });
-});
+// var sheetName = 'Sheet1'
+// 		var scriptProp = PropertiesService.getScriptProperties()
+
+// 		function intialSetup () {
+// 		  var activeSpreadsheet = SpreadsheetApp.getActiveSpreadsheet()
+// 		  scriptProp.setProperty('key', activeSpreadsheet.getId())
+// 		}
+
+// 		function doPost (e) {
+// 		  var lock = LockService.getScriptLock()
+// 		  lock.tryLock(10000)
+
+// 		  try {
+// 			var doc = SpreadsheetApp.openById(scriptProp.getProperty('key'))
+// 			var sheet = doc.getSheetByName(sheetName)
+
+// 			var headers = sheet.getRange(1, 1, 1, sheet.getLastColumn()).getValues()[0]
+// 			var nextRow = sheet.getLastRow() + 1
+
+// 			var newRow = headers.map(function(header) {
+// 			  return header === 'timestamp' ? new Date() : e.parameter[header]
+// 			})
+
+// 			sheet.getRange(nextRow, 1, 1, newRow.length).setValues([newRow])
+
+// 			return ContentService
+// 			  .createTextOutput(JSON.stringify({ 'result': 'success', 'row': nextRow }))
+// 			  .setMimeType(ContentService.MimeType.JSON)
+// 		  }
+
+// 		  catch (e) {
+// 			return ContentService
+// 			  .createTextOutput(JSON.stringify({ 'result': 'error', 'error': e }))
+// 			  .setMimeType(ContentService.MimeType.JSON)
+// 		  }
+
+// 		  finally {
+// 			lock.releaseLock()
+// 		  }
+// 		}
+
